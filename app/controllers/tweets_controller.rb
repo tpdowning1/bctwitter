95205b3fb4360a062136e6f7f0fd5c9d5f721931
Thomas <downinta@bc.edu>
2015-01-24 18:08:07 -0500

    I think we're done for the day

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,16 @@
 class TweetsController < ApplicationController
-	
+	before_action :authenticate_user!
+		
 	def new
 		@tweet = Tweet.new	
 	end
 
 	def create
-		@tweet = Tweet.create(tweet_params)
+
+		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
+		@tweet.save	
+
 		flash.now[:success] = "Tweet Created"
 		render 'new'
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124211655) do
+ActiveRecord::Schema.define(version: 20150124223334) do
 
   create_table "tweets", force: true do |t|
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
